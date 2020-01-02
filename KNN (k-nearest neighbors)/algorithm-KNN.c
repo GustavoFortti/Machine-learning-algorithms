99506bb1d5ecb492694e03ae1ab7b9ae9451cf0c
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-01-02 01:17:25 -0300

    Add files via upload

--- KNN (k-nearest neighbors)/algorithm-KNN.c
+++ KNN (k-nearest neighbors)/algorithm-KNN.c
@@ -30,14 +30,8 @@ int main(int argc, char const *argv[])
     int i;
     Node No_main[trein_size];
 
+    input(No_main, trein_size);
 
-    for ( i = 0; i < trein_size; i++)
-    {
-        scanf("%Lf %Lf %Lf %Lf %s", &No_main[i].x, &No_main[i].y, &No_main[i].w, &No_main[i].z, No_main[i].classe);
-        if (strcmp(No_main[i].classe, "Iris-setosa") == 0)No_main[i].index = 0;
-        else if (strcmp(No_main[i].classe, "Iris-versicolor") == 0)No_main[i].index = 1;
-        else if (strcmp(No_main[i].classe, "Iris-virginica") == 0)No_main[i].index = 2;
-    }
     Node *No_sub, No_aux;
     No_sub = &No_aux;
     int success = 0;
@@ -51,14 +45,14 @@ int main(int argc, char const *argv[])
         else if (strcmp(No_sub->classe, "Iris-versicolor") == 0)No_sub->index = 1;
         else if (strcmp(No_sub->classe, "Iris-virginica") == 0)No_sub->index = 2;
 
-        GotClass = classificationClass(No_main, No_sub, k, trein_size);
+        GotClass = classificationClass(No_main, No_sub, k, trein_size, i);
         if ( strcmp(GotClass, No_sub->classe) == 0)
         {
             success++;
         }
         else
         {
-            //printf("\n%s >>> %s", No_sub->classe, GotClass);
+            //printf("\n%s >> %s >> indice = %i", No_sub->classe, GotClass, i);
         }
         
     }
@@ -67,7 +61,7 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-char *classificationClass(Node No_main[], Node *No_sub, int k, int trein_size)
+char *classificationClass(Node No_main[], Node *No_sub, int k, int trein_size, int indice)
 {
     if ( k % 2 == 0)
     {
@@ -90,7 +84,17 @@ char *classificationClass(Node No_main[], Node *No_sub, int k, int trein_size)
         el[i].index = No_main[i].index;
     }
 
-    QuickSort(el, 0, trein_size);
+    SelectionSort(el, trein_size);
+    if ( indice == 2 || indice == 4  )
+    {
+        printf("\n%i\n", indice);
+        for ( i = 0; i < trein_size; i++)
+        {
+            printf("%Lf -> %i\n", el[i].dist, el[i].index);
+        }
+        printf("\n");
+    }
+    
 
     int cont_setosa = 0;
     int cont_versicolor = 0;
@@ -143,9 +147,28 @@ long double EuclideanDistance( Node main[], Node *sub, int i)
     return sqrt(dist);
 }
 
+void InserctionSort( Element v[], int size)
+{
+	long double escolhido = 0.0;
+    int j, i;
+    for ( i = 1; i < size; i++){
+        escolhido = v[i].dist;
+        escolhido = v[i].index;
+        j = i - 1;
+        while ((j >= 0) && ( v[j].dist > escolhido)){
+            v[j + 1].dist = v[j].dist;
+            v[j + 1].index = v[j].index;
+            j--;
+        }
+        v[j + 1].dist = escolhido;
+        v[j + 1].index = escolhido;
+    }
+}
+
 void QuickSort(Element v[], int began, int end)
 {
-    long i, j, pivo, aux;
+    int i, j;
+    long double pivo, aux;
 
     i = began;
     j = end - 1;
@@ -172,6 +195,18 @@ void QuickSort(Element v[], int began, int end)
     }
 }
 
+void SelectionSort( Element v[], int size)
+{
+	int i=0, j=0;
+    for ( i = 0; i < size-1; i++){
+        for ( j = i; j < size; j++){
+            if ( v[i].dist > v[j].dist){
+                swap( v, i, j);
+            }
+        }
+    }
+}
+
 void swap( Element v[], int i, int j)
 {
     long double aux_v = v[i].dist;
@@ -183,19 +218,20 @@ void swap( Element v[], int i, int j)
     v[j].index = aux_index;
 }
 
-// void input( Node No[], int size)
-// {
-//     FILE *file;
-//     int i;
-//     if (file = fopen("dataset.txt", "r") != NULL)
-//     {
-//         for ( i = 0; i < size; i++)
-//         {
-//             fscanf(file, "%Lf %Lf %Lf %Lf %s", &No[i].x, &No[i].y, &No[i].w, &No[i].z, No[i].classe);
-//             if (strcmp(No[i].classe, "Iris-setosa") == 0)No[i].index = 0;
-//             else if (strcmp(No[i].classe, "Iris-versicolor") == 0)No[i].index = 1;
-//             else if (strcmp(No[i].classe, "Iris-virginica") == 0)No[i].index = 2;
-//         }
-//         fclose(file);
-//     }
-// }
+void input( Node No[], int size)
+{
+    FILE *file;
+    file = fopen("dataset.txt", "r");
+    int i;
+    if (file != NULL)
+    {
+        for ( i = 0; i < size; i++)
+        {
+            fscanf(file, "%Lf %Lf %Lf %Lf %s", &No[i].x, &No[i].y, &No[i].w, &No[i].z, No[i].classe);
+            if (strcmp(No[i].classe, "Iris-setosa") == 0)No[i].index = 0;
+            else if (strcmp(No[i].classe, "Iris-versicolor") == 0)No[i].index = 1;
+            else if (strcmp(No[i].classe, "Iris-virginica") == 0)No[i].index = 2;
+        }
+        fclose(file);
+    }
+}
--- KNN (k-nearest neighbors)/knn.h
+++ KNN (k-nearest neighbors)/knn.h
@@ -4,9 +4,13 @@
 typedef struct node Node;
 typedef struct element Element;
 
-char *classificationClass(Node No_main[], Node *No_sub, int k, int trein_size);
-double EuclideanDistance( Node main[], Node *sub, int i);
+char *classificationClass(Node No_main[], Node *No_sub, int k, int trein_size, int indice);
+long double EuclideanDistance( Node main[], Node *sub, int i);
 void QuickSort( Element v[], int began, int end);
+void InserctionSort( Element v[], int size);
+void SelectionSort( Element v[], int size);
 void swap( Element v[], int i, int j);
+void input( Node No[], int size);
+
 
 #endif
