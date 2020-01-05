69e5da6c2ec353415440ee2286027c42dcdbbb85
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-01-04 23:03:29 -0300

    Add files via upload

--- DecisionTree/Iris-dataset/DecisionTree.h
+++ DecisionTree/Iris-dataset/DecisionTree.h
@@ -10,7 +10,7 @@ typedef struct InformationGain IG;
 void input(SubSet set[], int size);
 void CalcFeatures(SubSet set[], int size);
 void CalcMean(SubSet set[], int size);
-void CalcGI(SubSet set[], int size, int i);
+void CalcIG(SubSet set[], int size, int i);
 void CalcEntropy(SubSet set[],int size, int i, int index);
 
 #endif
