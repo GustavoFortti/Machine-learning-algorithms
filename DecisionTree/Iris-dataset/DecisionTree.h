b76909e163250e692b45d3a0b6e3fa1d8ca89d3a
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-01-06 19:07:19 -0300

    Add files via upload

--- DecisionTree/Iris-dataset/DecisionTree.h
+++ DecisionTree/Iris-dataset/DecisionTree.h
@@ -3,8 +3,9 @@
 
 #define size_pop     150
 #define size_ind     6
-#define size_var     4
+#define size_vet     4
 #define size_group   25
+#define size_plant   3
 
 typedef struct features Features;
 typedef struct nodo Nodo;
@@ -13,7 +14,7 @@ typedef struct InformationGain InfoGain;
 
 void input(SubSet set[]);
 void CalcFeatures(SubSet set[]);
-void CalcMean(SubSet set[], double mean[size_var][size_group]);
+void CalcMean(SubSet set[], double mean[size_vet][size_group]);
 void CalcIG(SubSet set[], int i, int index);
 
 #endif
