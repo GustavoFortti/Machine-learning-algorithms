8d64b5b243f75e74955941fe85b6fbcf47864a32
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-01-07 15:47:12 -0300

    Add files via upload

--- DecisionTree/Iris-dataset/DecisionTree.h
+++ DecisionTree/Iris-dataset/DecisionTree.h
@@ -16,5 +16,7 @@ void input(SubSet set[]);
 void CalcFeatures(SubSet set[]);
 void CalcMean(SubSet set[], double mean[size_vet][size_group]);
 void CalcIG(SubSet set[], int i, int index);
+void CrossValidation(SubSet set[], int i, int index, int block);
+
 
 #endif
