d3c1495a11902919e3b8379448c5dbf8b8a31199
GustavoFortti <53984056+GustavoFortti@users.noreply.github.com>
2020-03-04 20:08:21 -0300

    Add files via upload

--- Search_X_RVersion/main.R
+++ Search_X_RVersion/main.R
@@ -9,6 +9,12 @@ f <-function(x)
     return (20 - resultado)
   }
   else {
-    return( resultado - 20)
+    return( resultado -20)
   }
 }
+
+resultado = ga("real-value", fitness = f, lower=c(-20), upper=c(20), popSize = 10, maxiter = 20, monitor = T, names = c("a"))
+
+summary(resultado)$solution
+
+plot(resultado)
